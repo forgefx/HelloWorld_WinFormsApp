d3956404df9ad4b3c6ff18b45080adf3f6008e81
AdamKane <akane@forgefx.com>
2022-06-18 07:00:51 -1000

    Moved stuff to root directory, including build output.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("HelloWinForms")]
+[assembly: AssemblyDescription("HelloWorld non-Unity Windows application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyCompany("ForgeFX Simulations")]
+[assembly: AssemblyProduct("HelloWinForms")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2022.6.17.1324")]
+[assembly: AssemblyFileVersion("2022.6.17.1324")]
